d6b5d320c3efc7d689854eb3b51822eca4e76d41
temptado <jason1287712@gmail.com>
2021-10-09 20:48:15 -0400

    set up basic model types for exercise model

--- models/workout.js
+++ models/workout.js
@@ -1,3 +1,22 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+
+    totalDuration: Integer,
+
+    numExercises: Integer,
+
+    totalWeight: Integer,
+
+    totalSets: Integer,
+
+    totalReps: Integer,
+
+    totalDistance: Integer
+});
