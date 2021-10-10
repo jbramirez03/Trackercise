75b96398f3b53e520de4e9fc9549ffc15cd758c1
temptado <jason1287712@gmail.com>
2021-10-09 20:27:29 -0400

    started exercise schema fiile

---
+++ models/workout.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+    type: {
+        type: String,
+
+    },
+
+});
+
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+module.exports = Workout;
