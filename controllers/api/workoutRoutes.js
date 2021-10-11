5bbf66919ec33664ac02512d06f83254cd55c111
temptado <jason1287712@gmail.com>
2021-10-10 22:01:38 -0400

    created structure for routes in workoutRoutes file

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,3 +1,19 @@
 const router = require('express').Router();
 
+router.get('/workouts', (req, res) => {
+
+});
+
+router.put('/workouts', (req, res) => {
+
+});
+
+router.post('/workouts', (req, res) => {
+
+});
+
+router.get('./workouts/range', (req, res) => {
+
+});
+
 module.exports = router;
