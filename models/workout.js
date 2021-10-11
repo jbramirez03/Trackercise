d822bd5632733b0a18fb7e7a459b062663f17745
temptado <jason1287712@gmail.com>
2021-10-10 21:18:28 -0400

    finished basic structure for workout model with reference to exercises model

--- models/workout.js
+++ models/workout.js
@@ -2,8 +2,19 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema();
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Exercises"
+        }
+    ]
+});
 
-const Workout = mongoose.model('Workout', workoutSchema);
+const Workout = mongoose.model('Workouts', workoutSchema);
 
 module.exports = Workout;
