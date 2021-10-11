422f3d5f470e3a624d5216b5ca3d12e2bcbc649f
temptado <jason1287712@gmail.com>
2021-10-11 17:52:16 -0400

    completed route to get all workouts from mongo database

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
+const db = require('../../models');
 
-router.get('/workouts', (req, res) => {
+
+router.get('/workouts', async (req, res) => {
+    try {
+        const workouts = await db.Workout.find({});
+        console.log(workouts);
+        res.status(200).json(workouts);
+    } catch (err) {
+        err && console.log(err);
+    }
 
 });
 
