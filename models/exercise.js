1acaf5e41c36925f3469ef9623a6c1159ceb03bb
temptado <jason1287712@gmail.com>
2021-10-10 21:23:59 -0400

    finished basic structure for exercise model

--- models/exercise.js
+++ models/exercise.js
@@ -2,6 +2,26 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const exerciseSchema = new Schema();
+const exerciseSchema = new Schema({
+    type: {
+        type: String
+    },
+    name: {
+        type: String,
+        required: 'must give name of exercise'
+    },
+    duration: {
+        type: Number
+    },
+    weight: {
+        type: Number
+    },
+    reps: {
+        type: Number
+    },
+    sets: {
+        type: Number
+    }
+});
 
 const Exercise = mongoose.model('Exercises', exerciseSchema);
