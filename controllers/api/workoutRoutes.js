7dd3e4047f46455c341286703a4c85a26c2493f3
temptado <jason1287712@gmail.com>
2021-10-14 20:15:43 -0400

    added aggregate to show total duration

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -4,7 +4,11 @@ const db = require('../../models');
 
 router.get('/', async (req, res) => {
     try {
-        const workouts = await db.Workout.find({});
+        const workouts = await db.Workout.aggregate([
+            {
+                $addFields: { totalDuration: { $sum: "$exercises.duration" } }
+            }
+        ]);
         res.status(200).json(workouts);
     } catch (err) {
         err && res.status(500).json(err);
